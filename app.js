a517b3ef08a55bea0be808e223e40282f45febd5
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-09-09 15:05:47 +0300

    Change port to heroku port.

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const bookRoutes = require('./routes/bookRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bookRoutes = require("./routes/bookRoutes");
 
 // express app
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://admin:admin@cluster0.o4cmt.mongodb.net/bookshelf?retryWrites=true&w=majority";
+const dbURI =
+  "mongodb+srv://admin:admin@cluster0.o4cmt.mongodb.net/bookshelf?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(morgan('dev'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
 // routes
-app.get('/', (req, res) => {
-  res.redirect('/books');
+app.get("/", (req, res) => {
+  res.redirect("/books");
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact" });
 });
 
 // book routes
-app.use('/books', bookRoutes);
+app.use("/books", bookRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
